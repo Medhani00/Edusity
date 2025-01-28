3c44003ed7c8e6289eb43c238e437f063f6efd54
Medhani00 <vihangiweersingha200025@gmail.com>
2025-01-28 20:16:48 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Navbar/Hero/Hero'
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      {/* <Hero/> */}
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
+
